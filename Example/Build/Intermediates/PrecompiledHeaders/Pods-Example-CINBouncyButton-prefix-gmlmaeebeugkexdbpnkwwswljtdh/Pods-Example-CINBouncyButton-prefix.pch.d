2e8aa872363138c116ffac4781c2f54585375b82
Max Kramer <max@maxkramer.co>
2014-05-16 23:34:28 +0100

    Fixes #1. Have sent PR to Cocoapods/Specs repo

---
+++ Example/Build/Intermediates/PrecompiledHeaders/Pods-Example-CINBouncyButton-prefix-gmlmaeebeugkexdbpnkwwswljtdh/Pods-Example-CINBouncyButton-prefix.pch.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Max/Desktop/CINBouncyButton/Example/Pods/Pods-Example-CINBouncyButton-prefix.pch \
+  /Users/Max/Desktop/CINBouncyButton/Example/Pods/Pods-Example-environment.h
